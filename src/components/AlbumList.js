b07bd5d5ff8cc4c875c8e1472b6ca3849f15db94
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-01-29 12:28:54 +0100

    card child, api, imports...

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Albumlist</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: [],
+    };
+  }
+  componentDidMount() {
+    this.fetchAlbums();
+  }
+
+  fetchAlbums = () => {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(
+      response => {
+        this.setState({
+          albums: response.data,
+        });
+        console.log(response);
+      },
+      error => {
+        console.log(error);
+        throw error;
+      },
+    );
+  };
+
+  render() {
+    let albums = [];
+    let stateAlbums = this.state.albums;
+    stateAlbums.forEach((album, i) => {
+      albums.push(<AlbumDetail key={i} album={album} />);
+    });
+    return <View>{albums}</View>;
+  }
+}
 
 export default AlbumList;
